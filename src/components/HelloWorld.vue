66064ddde4eebcc9247d3dc6bd5a9365fd0e426e
Takafumi Maeda <takafumi.maeda@monstar-lab.com>
2025-01-06 08:02:09 +0900

    調整

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,25 +2,26 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-text-field
-          v-model="newTask"
-          label="新しいタスクを追加"
-          @keyup.enter="addTask"
-        />
-        <v-btn @click="addTask">追加</v-btn>
+        <v-text-field v-model="newTask" label="新しいタスクを追加">
+          <template v-slot:append>
+            <v-btn @click="addTask" color="primary">追加</v-btn>
+          </template>
+        </v-text-field>
       </v-col>
     </v-row>
     <v-list>
       <v-list-item-group>
         <v-list-item v-for="(task, index) in tasks" :key="index">
           <v-list-item-content>
-            <v-list-item-title>{{ task }}</v-list-item-title>
+            <v-row>
+              <v-col>{{ task }}</v-col>
+              <v-col class="d-flex justify-end">
+                <v-btn @click="removeTask(index)" icon>
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-list-item-content>
-          <v-list-item-action>
-            <v-btn @click="removeTask(index)" icon>
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
         </v-list-item>
       </v-list-item-group>
     </v-list>
@@ -29,9 +30,9 @@
 
 <script setup>
 import { ref } from "vue";
-
 const newTask = ref("");
 const tasks = ref([]);
+tasks.value = ["タスク1", "タスク2", "タスク3"];
 
 const addTask = () => {
   if (newTask.value.trim()) {
